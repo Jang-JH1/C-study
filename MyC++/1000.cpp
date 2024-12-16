b240c7d096df7367abf63fe12b50c605878583f7
JH <71679902+Jang-JH1@users.noreply.github.com>
2024-12-16 10:31:48 +0900

    Add files via upload

---
+++ MyC++/1000.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+//typedef vector<pair<string, int>> pairlist_t;
+//typedef string text_t;
+using text_t = string;
+
+int main()
+{
+    std::cout<<"\n";
+    
+    string damm = "Damm!";
+    text_t god = "GOD!";
+
+    cout << damm << "\n";
+    cout << god << "\n";
+
+    return 0;
+}
